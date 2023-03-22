6dc64e84ea1d64783097f73aec3437d6ca706fe9
pudge <1184321378@qq.com>
2023-03-22 16:53:47 +0800

    1.generator  auth 工程代码 补充

---
+++ iot-generator/src/main/java/com/pudge/cn/iot/system/auth/entity/RolePermission.java
@@ -0,0 +1,33 @@
+package com.pudge.cn.iot.system.auth.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableField;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author pudge
+ * @since 2023-03-22
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@TableName("role_permission")
+public class RolePermission implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "pid", type = IdType.ASSIGN_ID)
+    private Integer pid;
+
+    @TableField("rid")
+    private Integer rid;
+
+
+}
---
+++ iot-generator/src/main/java/com/pudge/cn/iot/system/auth/service/IRolePermissionService.java
@@ -0,0 +1,16 @@
+package com.pudge.cn.iot.system.auth.service;
+
+import com.pudge.cn.iot.system.auth.entity.RolePermission;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author pudge
+ * @since 2023-03-22
+ */
+public interface IRolePermissionService extends IService<RolePermission> {
+
+}
